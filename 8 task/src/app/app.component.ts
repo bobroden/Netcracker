7055de12de9eece91bd8ae2bc39b44a688c723bc
bobro <bobrow.bobro2015@yandex.ru>
2021-04-24 17:52:59 +0300

    fix: save data in filter

--- 8 task/src/app/app.component.ts
+++ 8 task/src/app/app.component.ts
@@ -21,6 +21,7 @@ export class AppComponent {
 	searched = "";
 	studentsList: Student[] = students;
 	studentsList2: Student[];
+	studentsList3: Student[] = this.studentsList;
 
 	sortname = false;
 	sortsurname = false;
@@ -30,62 +31,67 @@ export class AppComponent {
 	modalwindow = false;
 	id: Number;
 
-	ScoreInput: Number;
-	FilterScore: String = ">";
+	scoreInput: Number;
+	filterScore: String = ">";
 
-	DateInput: Date;
-	FilterDate: String = ">";
+	dateInput: Date;
+	filterDate: String = ">";
 
-	Show(): void {
+	show(): void {
 		this.isred = !this.isred;
 	}
 
 
-	ChangeScoreInput(e: Event): void {
-		this.ScoreInput = +(<HTMLInputElement>e.target).value;
+	changeScoreInput(e: Event): void {
+		this.scoreInput = +(<HTMLInputElement>e.target).value;
 	}
 
-	ChangeFilterScore(e: Event): void {
-		this.FilterScore = (<HTMLInputElement>e.target).value;
+	changeFilterScore(e: Event): void {
+		this.filterScore = (<HTMLInputElement>e.target).value;
 	}
 
-	ChangeDateInput(e: Event): void {
-		this.DateInput = new Date((<HTMLInputElement>e.target).value);
-		this.DateInput.setHours(0);
+	changeDateInput(e: Event): void {
+		this.dateInput = new Date((<HTMLInputElement>e.target).value);
+		this.dateInput.setHours(0);
 	}
 
-	ChangeFilterDate(e: Event): void {
-		this.FilterDate = (<HTMLInputElement>e.target).value;
+	changeFilterDate(e: Event): void {
+		this.filterDate = (<HTMLInputElement>e.target).value;
 	}
 
-	Filter(): void {
-		if (this.ScoreInput && this.FilterScore) {
-			if (this.FilterScore === "=") {
-				this.studentsList2 = this.studentsList.filter(student => +student.Average_score === this.ScoreInput);
-				this.studentsList = this.studentsList2;
-			}
-			if (this.FilterScore === ">") {
-				this.studentsList2 = this.studentsList.filter(student => +student.Average_score > this.ScoreInput);
-				this.studentsList = this.studentsList2;
-			}
-			if (this.FilterScore === "<") {
-				this.studentsList2 = this.studentsList.filter(student => +student.Average_score < this.ScoreInput);
-				this.studentsList = this.studentsList2;
-			}
+	filter(): void {
+		if(this.studentsList3) {
+			this.studentsList = this.studentsList3;
 		}
-		if (this.DateInput && this.FilterDate) {
-			if (this.FilterDate === "=") {
-				this.studentsList2 = this.studentsList.filter(student => new Date(student.Date_of_birth).getTime() === this.DateInput.getTime());
-				this.studentsList = this.studentsList2;
+		if (this.scoreInput && this.filterScore) {
+			if (this.filterScore === "=") {
+				this.studentsList2 = this.studentsList.filter(student => +student.Average_score === this.scoreInput);
 			}
-			if (this.FilterDate === ">") {
-				this.studentsList2 = this.studentsList.filter(student => new Date(student.Date_of_birth).getTime() > this.DateInput.getTime());
-				this.studentsList = this.studentsList2;
+			else if (this.filterScore === ">") {
+				this.studentsList2 = this.studentsList.filter(student => +student.Average_score > this.scoreInput);
 			}
-			if (this.FilterDate === "<") {
-				this.studentsList2 = this.studentsList.filter(student => new Date(student.Date_of_birth).getTime() < this.DateInput.getTime());
-				this.studentsList = this.studentsList2;
+			else if (this.filterScore === "<") {
+				this.studentsList2 = this.studentsList.filter(student => +student.Average_score < this.scoreInput);
 			}
+			this.studentsList = this.studentsList2;
+		}
+		if (this.dateInput && this.filterDate) {
+			if (this.filterDate === "=") {
+				this.studentsList2 = this.studentsList.filter(student => new Date(student.Date_of_birth).getTime() === this.dateInput.getTime());
+			}
+			else if (this.filterDate === ">") {
+				this.studentsList2 = this.studentsList.filter(student => new Date(student.Date_of_birth).getTime() > this.dateInput.getTime());
+			}
+			else if (this.filterDate === "<") {
+				this.studentsList2 = this.studentsList.filter(student => new Date(student.Date_of_birth).getTime() < this.dateInput.getTime());
+			}
+			this.studentsList = this.studentsList2;
+		}
+	}
+
+	reset(): void {
+		if(this.studentsList3) {
+			this.studentsList = this.studentsList3;
 		}
 	}
 
@@ -96,30 +102,31 @@ export class AppComponent {
 		return false;
 	}
 
-	Search(str: String): boolean {
+	search(str: String): boolean {
 		if (this.searched === str.slice(0, this.searched.length) && this.searched.length !== 0) {
 			return true;
 		}
 		return false;
 	}
 
-	ChangeStr(e: Event): void {
+	changeStr(e: Event): void {
 		this.searched = (<HTMLInputElement>e.target).value;
 	}
 
-	Delete(id: Number): void {
+	delete(id: Number): void {
 		this.modalwindow = true;
 		this.id = id;
 	}
 
-	DeleteRow(isDelete: Boolean): void {
+	deleteRow(isDelete: Boolean): void {
 	if (isDelete === true) {
+		this.studentsList3 = this.studentsList3.filter(student => student.Date_of_birth != this.studentsList[+this.id].Date_of_birth);
 		this.studentsList.splice(+this.id, 1);
 	}
 		this.modalwindow = false;
 	}
 
-	SortDate(): void {
+	sortDate(): void {
 		if (this.sortdate) {
 			this.studentsList.sort((a, b) => new Date(b.Date_of_birth).getTime() - new Date(a.Date_of_birth).getTime());
 			this.sortdate = false;
@@ -129,7 +136,7 @@ export class AppComponent {
 		}
 	}
 
-	SortName(): void {
+	sortName(): void {
 		if (this.sortname) {
 			this.studentsList.sort((a, b) => a.Name > b.Name ? -1 : 1);
 			this.sortname = false;
@@ -139,7 +146,7 @@ export class AppComponent {
 		}
 	}
 
-	SortSurname(): void {
+	sortSurname(): void {
 		if (this.sortsurname) {
 			this.studentsList.sort((a, b) => a.Surname > b.Surname ? -1 : 1);
 			this.sortsurname = false;
@@ -149,7 +156,7 @@ export class AppComponent {
 		}
 	}
 
-	SortPatronymic(): void {
+	sortPatronymic(): void {
 		if (this.sortpatronymic) {
 			this.studentsList.sort((a, b) => a.Patronymic > b.Patronymic ? -1 : 1);
 			this.sortpatronymic = false;
@@ -159,7 +166,7 @@ export class AppComponent {
 		}
 	}
 
-	SortScore(): void {
+	sortScore(): void {
 		if (this.sortscore) {
 			this.studentsList.sort((a, b) => +a.Average_score - +b.Average_score);
 			this.sortscore = false;
